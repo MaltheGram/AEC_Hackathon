edbf2da7c24f2d6c2d09b8830ddd368b246d8086
thorminathor4 <thorfa4444@gmail.com>
2021-11-06 20:12:55 +0100

    Some window stuff

--- Hackathon app/src/Main.java
+++ Hackathon app/src/Main.java
@@ -1,13 +1,19 @@
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import java.util.ArrayList;
 
 public class Main
 {
-    final static double minRoomHeight=2.3;
+    final static double minRoomHeight=2300;
+    static JFrame window;
     public static void main(String[] args){
-        ArrayList<Wall> walls=getWalls();
+        //ArrayList<Wall> walls=getWalls();
         //ArrayList<Pipe> pipes=getPipes();
-
+        makeFilereadWindow();
+        //makeMessageWindow();
     }
+
     public static ArrayList<Wall> getWalls(){
         String filePath="/Users/Thor/IdeaProjects/AEC_Hackathon/Hackathon app/src/AEC_Hackathon_Fix Revit.csv";
         ArrayList<String> fileLines=FileReader.fileToStringList(filePath);
@@ -20,6 +26,7 @@ public class Main
         }
         return walls;
     }
+
     public static ArrayList<Pipe> getPipes(){
         String filePath="/Users/Thor/IdeaProjects/AEC_Hackathon/Hackathon app/src/AEC hackathon Piping data .csv";
         ArrayList<String> fileLines=FileReader.fileToStringList(filePath);
@@ -31,5 +38,47 @@ public class Main
             pipes.add(pipe);
         }
         return pipes;
+    }
+
+    public static void makeFilereadWindow(){
+
+        JLabel label=new JLabel("Enter filepath:");
+
+        JButton checkButton=new JButton();
+        checkButton.setText("Check");
+
+        JPanel panel=new JPanel();
+        panel.add(label);
+        panel.add(new JTextField(20));
+        panel.add(checkButton);
+
+
+        JFrame frame=new JFrame("Check Regulations");
+        frame.add(panel);
+        frame.setSize(600,400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void makeMessageWindow(){
+        JPanel middlePanel = new JPanel ();
+        middlePanel.setBorder ( new TitledBorder( new EtchedBorder(), "Messages" ) );
+        JTextArea display = new JTextArea ( 16, 58 );
+        String allText="";
+        for(int i=1;i<=50;i++) allText+=("Error "+i+" ").repeat(15)+"\n";
+        display.setText(allText);
+        display.setEditable ( false );
+        JScrollPane scroll = new JScrollPane ( display );
+        scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+
+        middlePanel.add ( scroll );
+
+        JFrame frame = new JFrame ();
+        frame.add ( middlePanel );
+        frame.pack();
+        frame.setLocationRelativeTo ( null );
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible ( true );
     }
 }
